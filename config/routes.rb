b7e795ca37247a9d8a97227d8b791468f5c8760a
bweegee <bw33g33@gmail.com>
2019-02-19 17:04:44 -0700

    model, controller, view updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'people/index'
-  get 'people/show'
-  get 'people/new'
+  resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
